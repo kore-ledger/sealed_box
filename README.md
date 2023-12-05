1be49e83ad9f031e41d0e6303189623cca1adf84
Antonio Estevez <aestgar62gmail.com>
2023-12-05 19:12:15 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sealed_box
+
+[![Rust](https://img.shields.io/badge/Rust-v1.73.0-orange)](https://www.rust-lang.org/)
+[![License](https://img.shields.io/badge/License-Apache--2.0-green)](https://github.com/kore-ledger/sealed_box/blob/v0.1.0/LICENSE)
+![Build & Test](https://github.com/kore-ledger/sealed_box/actions/workflows/build.yml/badge.svg)
---
+++ src/lib.rs
@@ -0,0 +1,121 @@
+// licence: Apache 2.0
+
+//! # sealed_box library
+//! 
+//! This library provides a set of functions to encrypt and decrypt content using the
+//! Rust implementation of [NaCl’s](https://nacl.cr.yp.to/) 
+//! [crypto_box](https://nacl.cr.yp.to/box.html) primitive.
+//!
+
+#![warn(missing_docs)]
+
+use anyhow::Error;
+use blake2::{Blake2s256, Digest, digest::{Update, generic_array::GenericArray}};
+use crypto_box::{aead::Aead, ChaChaBox, SecretKey, PublicKey};
+
+/// Public key size
+const PUBLIC_KEY_SIZE: usize = 32;
+
+/// Seal box
+pub fn seal_box(
+    message: &[u8],
+    public_key: &[u8],
+    secret_key: Option<&[u8]>,
+) -> Result<Vec<u8>, Error> {
+    let sk = if let Some(secret_key) = secret_key {
+        SecretKey::from_slice(secret_key)?
+    } else {
+        SecretKey::generate(&mut rand::rngs::OsRng)
+    };
+    let pk = PublicKey::from(&sk);
+    let rk = PublicKey::from_slice(public_key)?;
+    let nonce = &Blake2s256::new().chain(&pk.as_bytes()).chain(&rk.as_bytes()).finalize()[..24];
+    let cipher = encrypt_box(message, &rk, &sk, nonce)?;
+    println!("cipher: {:?}", cipher);
+    Ok([pk.as_bytes().to_vec(), cipher].concat())
+}
+
+/// Encrypt box.
+fn encrypt_box(
+    message: &[u8],
+    public_key: &PublicKey,
+    secret_key: &SecretKey,
+    nonce: &[u8],
+) -> Result<Vec<u8>, Error> {
+    let nonce = GenericArray::from_slice(nonce);
+    Ok(ChaChaBox::new(public_key, secret_key).encrypt(nonce, message)
+        .map_err(|_| SealedBoxError::Aead)?)
+}
+
+/// Unseal box.
+pub fn unseal_box(
+    cipher: &[u8],
+    secret_key: &SecretKey,
+) -> Result<Vec<u8>, Error> {
+    if cipher.len() < PUBLIC_KEY_SIZE {
+        Err(SealedBoxError::BoxToSmall.into())
+    } else {
+        let pk_bytes: [u8; PUBLIC_KEY_SIZE] = cipher[..PUBLIC_KEY_SIZE].try_into()
+            .map_err(|_| SealedBoxError::BoxToSmall)?;
+        let pk = PublicKey::from_slice(&pk_bytes)?;
+        let rk = PublicKey::from(secret_key);
+        let nonce = &Blake2s256::new().chain(&pk.as_bytes()).chain(&rk.as_bytes()).finalize()[..24];
+        let message = decrypt_box(&cipher[PUBLIC_KEY_SIZE..], &pk, &secret_key, nonce)?;
+        Ok(message)    
+    }
+}
+
+/// Decrypt box.
+fn decrypt_box(
+    cipher: &[u8],
+    public_key: &PublicKey,
+    secret_key: &SecretKey,
+    nonce: &[u8],
+) -> Result<Vec<u8>, Error> {
+    println!("cipher: {:?}", cipher.to_vec());
+    let nonce = GenericArray::from_slice(nonce);
+    let enc = ChaChaBox::new(public_key, secret_key);
+    let result = enc.decrypt(nonce, cipher);
+    println!("result: {:?}", result);
+    Ok(ChaChaBox::new(public_key, secret_key).decrypt(nonce, cipher)
+        .map_err(|_| SealedBoxError::Aead)?)
+}
+
+/// Error por AEAD
+#[derive(Debug,)]
+pub enum SealedBoxError {
+    /// AEAD error
+    Aead,
+    /// Box size error
+    BoxToSmall,
+}
+
+impl std::fmt::Display for SealedBoxError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+           SealedBoxError::Aead => f.write_str("Aead error"),
+           SealedBoxError::BoxToSmall => f.write_str("Box to small"),
+        }
+    }
+}
+
+impl std::error::Error for SealedBoxError {}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_seal_unseal() {
+        let message = b"Hello world!";
+        
+        let fsk = SecretKey::from_slice(b"0123456789abcdef0123456789abcdef").unwrap();
+        let fpk = PublicKey::from(&fsk);
+
+        let cipher = seal_box(message, fpk.as_bytes(), Some(b"fedcba9876543210fedcba9876543210")).unwrap();
+        let message2 = unseal_box(&cipher, &fsk).unwrap();
+        assert_eq!(message, &message2[..]);
+    }
+
+}
